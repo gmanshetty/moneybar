8b02740888cd56ebd0db0e8b382fa0985a31a630
Gmanshetty <gmanshetty@ymail.com>
2011-02-15 23:39:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moneybar::Application.config.secret_token = 'a137543406f8f15438eb7456c5353b1959cbee0cad2c687d7b42107756e9c3ba0d4d883cd02cf542bf3acd75a5e0d2f5b1e99312360cdc163824f4fce28e1852'
